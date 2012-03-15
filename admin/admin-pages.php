6ce308d188f00239ad173737579fda18383de071
businessdirectoryplugin <businessdirectoryplugin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-15 15:12:37 +0000

    Update to 2.0, fixed 22 bugs, updated admin interface, improved usability and UI, simplified workflow
    git-svn-id: https://plugins.svn.wordpress.org/business-directory-plugin/trunk@519401 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/admin-pages.php
@@ -0,0 +1,95 @@
+<?php
+function wpbdp_admin_sidebar() {
+    return wpbdp_render_page(WPBDP_PATH . 'admin/templates/sidebar.tpl.php');
+}
+
+function wpbdp_admin_header($title_=null, $id=null) {
+	global $title;
+
+	if (!$title_) $title_ = $title;
+    return wpbdp_render_page(WPBDP_PATH . 'admin/templates/header.tpl.php', array('page_title' => $title_, 'page_id' => $id));
+}
+
+
+function wpbdp_admin_footer()
+{
+	$html = '<!--</div>--></div><br class="clear" /></div>';
+	return $html;
+}
+
+/* Admin home screen setup begin */
+function wpbusdirman_home_screen()
+{
+	global $wpbdmposttypecategory,$wpbusdirmanconfigoptionsprefix,$wpbusdirman_hastwocheckoutmodule,$wpbusdirman_haspaypalmodule,$wpbusdirman_hasgooglecheckoutmodule;
+	$wpbusdirman_config_options=get_wpbusdirman_config_options();
+	$listyle="style=\"width:auto;float:left;margin-right:5px;\"";
+	$listyle2="style=\"width:200px;float:left;margin-right:5px;\"";
+	$html = '';
+
+	$html .= wpbdp_admin_header();
+	$wpbusdirman_myterms = get_terms($wpbdmposttypecategory, 'orderby=name&hide_empty=0');
+	if($wpbusdirman_myterms)
+	{
+		foreach($wpbusdirman_myterms as $wpbusdirman_myterm)
+		{
+			$wpbusdirman_postcatitems[]=$wpbusdirman_myterm->term_id;
+		}
+	}
+	if(!empty($wpbusdirman_postcatitems))
+	{
+		foreach($wpbusdirman_postcatitems as $wpbusdirman_postcatitem)
+		{
+			$wpbusdirman_tlincat=&get_term( $wpbusdirman_postcatitem, $wpbdmposttypecategory, '', '' );
+			$wpbusdirman_totallistingsincat[]=$wpbusdirman_tlincat->count;
+		}
+		$wpbusdirman_totallistings=array_sum($wpbusdirman_totallistingsincat);
+		$wpbusdirman_totalcatsindir=count($wpbusdirman_postcatitems);
+	}
+	else
+	{
+		$wpbusdirman_totallistings=0;
+		$wpbusdirman_totalcatsindir=0;
+	}
+	$html .= "<h3 style=\"padding:10px;\">" . __("Options Menu","WPBDM") . "</h3><p>" . __("You are using version","WPBDM") . " <b>" . WPBDP_Plugin::VERSION . "</b> </p>";
+if( $wpbusdirman_config_options[$wpbusdirmanconfigoptionsprefix.'_settings_config_40'] == "yes"
+		&& $wpbusdirman_config_options[$wpbusdirmanconfigoptionsprefix.'_settings_config_41'] == "yes"
+			&& $wpbusdirman_config_options[$wpbusdirmanconfigoptionsprefix.'_settings_config_43'] == "yes"
+				&& $wpbusdirman_config_options[$wpbusdirmanconfigoptionsprefix.'_settings_config_21'] == "yes" )
+						{
+							$html .= "<p style=\"padding:10px;background:#ff0000;color:#ffffff;font-weight:bold;\">";
+							$html.=__("You have payments turned on but all your gateways are set to hidden. Your system will run as if payments are turned off until you fix the problem. To fix go to <i>Manage options > Payment</i> and unhide at least 1 payment gateway, or if it is your intention not to charge a payment fee set payments to off instead of on.","WPBDM");
+							$html.="</p>";
+						}
+	$html .= "<ul><li class=\"button\" $listyle><a style=\"text-decoration:none;\" href=\"?page=wpbdp_settings\">" . __("Configure/Manage Options","WPBDM") . "</a></li>";
+	$html .= "<li class=\"button\" $listyle><a style=\"text-decoration:none;\" href=\"?page=wpbdman_c2\">" . __("Setup/Manage Fees","WPBDM") . "</a></li>";
+	$html .= "<li class=\"button\" $listyle><a style=\"text-decoration:none;\" href=\"?page=wpbdman_c3\">" . __("Setup/Manage Form Fields","WPBDM") . "</a></li>";
+	if($wpbusdirman_config_options[$wpbusdirmanconfigoptionsprefix.'_settings_config_31'] == "yes")
+	{
+		$html .= "<li class=\"button\" $listyle><a style=\"text-decoration:none;\" href=\"" . admin_url(sprintf('edit.php?post_type=%s&wpbdmfilter=pendingupgrade', WPBDP_Plugin::POST_TYPE)) . "\">" . __("Featured Listings Pending Upgrade","WPBDM") . "</a></li>";
+	}
+	if($wpbusdirman_config_options[$wpbusdirmanconfigoptionsprefix.'_settings_config_21'] == "yes")
+	{
+		$html .= "<li class=\"button\" $listyle><a style=\"text-decoration:none;\" href=\"" . admin_url(sprintf('edit.php?post_type=%s&wpbdmfilter=unpaid', WPBDP_Plugin::POST_TYPE)) . "\">" . __("Manage Paid Listings","WPBDM") . "</a></li>";
+	}
+	$html .= "</ul><br /><div style=\"clear:both;\"></div><ul>";
+	$html .= "<li $listyle2>" . __("Listings in directory","WPBDM") . ": (<b>$wpbusdirman_totallistings</b>)</li>";
+	$html .= "<li $listyle2>" . __("Categories In Directory","WPBDM") . ": (<b>$wpbusdirman_totalcatsindir</b>)</li></ul><div style=\"clear:both;\"></div>";
+	if($wpbusdirman_config_options[$wpbusdirmanconfigoptionsprefix.'_settings_config_26'] == "yes")
+	{
+		$html .= "<h4>" . __("Tips for Use and other information","WPBDM") . "</h4>";
+		$html .= "<ol>";
+		if($wpbusdirman_config_options[$wpbusdirmanconfigoptionsprefix.'_settings_config_21'] == "yes")
+		{
+			$html .= "<li>" . __("Leave default post status set to pending to avoid misuse","WPBDM") . "<br />" . __("Listing payment status is not automatically updated after payment has been made. For this reason it is best to leave the listing default post status set to pending so you can verify that a listing has been paid for before it gets publised.","WPBDM") . "</li>";
+			$html .= "<li>" . __("Valid Merchant ID and sandbox seller ID required for Google checkout payment processing ","WPBDM") . "</li>";
+		}
+		$html .= "<li>" . __("The plugin uses it's own page template to display single posts and category listings. You can modify the templates to make them match your site by editing the template files in the posttemplates folder which you will find inside the plugin folder. ","WPBDM") . "</li>";
+		$html .= "<li>" . __("To protect user privacy Email addresses are not displayed in listings. ","WPBDM") . "</li>";
+		$html .= "<li>" . __("reCaptcha human verification is built into the plugin contact form but comes turned off by default. To use it you need to turn it on. You also need to have a recaptcha public and private key. To obtain these visit recaptcha.net then enter the keys into he related boxes from the manage options page. ","WPBDM") . "</li>";
+		$html .= "<li>" . __("You can hide these tips by going to Configure/Manage Options and checking the box next to 'Hide tips for use and other information'","WPBDM") .  "</li></ol>";
+	}
+	$html .= wpbdp_admin_footer();
+
+	echo $html;
+}
+/* Admin home screen setup end */
---
+++ admin/templates/sidebar.tpl.php
@@ -0,0 +1,58 @@
+<?php
+?>
+<div class="sidebar">
+	<div class="meta-box-sortables metabox-holder ui-sortable" id="side-sortables">
+		<!-- Like this plugin? -->
+		<div class="postbox">
+			<h3 class="hndle"><span>Like this plugin?</span></h3>
+			<div class="inside">
+				<p>Why not do any or all of the following:</p>
+				<ul>
+				    <li class="li_link"><a href="http://wordpress.org/extend/plugins/business-directory-plugin/">Give it a good rating on WordPress.org.</a></li>
+				    <li class="li_link"><a href="http://wordpress.org/extend/plugins/business-directory-plugin/">Let other people know that it works with your WordPress setup.</a></li>
+				    <li class="li_link"><a href="http://businessdirectoryplugin.com/premium-modules/">Buy a Premium Module</a></li>
+			    </ul>
+			</div>
+		</div>
+
+		<!-- Premium modules -->
+		<div class="postbox premium-modules">
+			<h3 class="hndle"><span>Get a Premium Module</span></h3>
+			<div class="inside">
+				<ul>
+				    <li class="li_link"><a href="http://businessdirectoryplugin.com/premium-modules/paypal-module/">PayPal Payment Gateway Module</a></li>
+				    <li class="li_link"><a href="http://businessdirectoryplugin.com/premium-modules/2checkout-module/">2Checkout Payment Gateway Module</a></li>
+				    <li class="li_link"><a href="http://businessdirectoryplugin.com/premium-modules/business-directory-combo-pack/">Single Site License Combo Pack</a></li>
+				    <li class="li_link"><a href="http://businessdirectoryplugin.com/premium-modules/business-directory-combo-pack-multi-site/">Multi Site License Combo Pack</a></li>
+			    </ul>
+			</div>
+		</div>
+
+		<!-- Installed modules -->
+		<div class="postbox installed-modules">
+			<h3 class="hndle"><span>Installed Modules</span></h3>
+			<div class="inside">
+<!-- 				"Module: Installed/Not
+Installed", where Module refers to the name of any premium module. -->
+				<ul>
+				    <li class="li_link">
+				    	<a href="http://businessdirectoryplugin.com/premium-modules/paypal-module/">PayPal Payment Gateway</a>:<br />
+				    	<?php echo wpbdp()->has_module('paypal') ? _x('Installed', 'admin sidebar', 'WPBDM') : _x('Not Installed', 'admin sidebar', 'WPBDM'); ?>
+				    </li>
+				    <li class="li_link">
+				    	<a href="http://businessdirectoryplugin.com/premium-modules/2checkout-module/">2Checkout Payment Gateway</a>:<br />
+				    	<?php echo wpbdp()->has_module('2checkout') ? _x('Installed', 'admin sidebar', 'WPBDM') : _x('Not Installed', 'admin sidebar', 'WPBDM'); ?>
+				    </li>
+			    </ul>
+			</div>
+		</div>
+
+		<!-- Support -->
+		<div class="postbox">
+			<h3 class="hndle"><span>Found a bug? Need support?</span></h3>
+			<div class="inside">
+				<p>If you've found a bug or need support <a href="http://businessdirectoryplugin.com/forums/" target="_blank">visit the forums!</a></p>
+			</div>
+		</div>
+	</div>
+</div>
