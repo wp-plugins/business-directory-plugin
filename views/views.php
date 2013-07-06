dd2eac36a5bf00a2608c982f47824b085db658f9
businessdirectoryplugin <businessdirectoryplugin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-06 01:46:01 +0000

    3.1.1 release
    git-svn-id: https://plugins.svn.wordpress.org/business-directory-plugin/trunk@736919 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/views.php
+++ views/views.php
@@ -66,10 +66,7 @@ class WPBDP_DirectoryController {
                 break;
             case 'editlisting':
             case 'submitlisting':
-                require_once( WPBDP_PATH . 'views/submit-listing.php' );
-                $submit_page = new WPBDP_SubmitListingPage( isset( $_REQUEST['listing_id'] ) ? $_REQUEST['listing_id'] : 0 );
-                return $submit_page->dispatch();
-
+                return $this->submit_listing();
                 break;
             case 'sendcontactmessage':
                 return $this->send_contact_message();
@@ -232,6 +229,12 @@ class WPBDP_DirectoryController {
         wp_reset_query();
 
         return $html;
+    }
+
+    public function submit_listing() {
+        require_once( WPBDP_PATH . 'views/submit-listing.php' );
+        $submit_page = new WPBDP_SubmitListingPage( isset( $_REQUEST['listing_id'] ) ? $_REQUEST['listing_id'] : 0 );
+        return $submit_page->dispatch();
     }
 
     /*
@@ -547,7 +550,7 @@ class WPBDP_DirectoryController {
 
         query_posts( array(
             'post_type' => WPBDP_POST_TYPE,
-            'posts_per_page' => 10,
+            'posts_per_page' => 0,
             'paged' => get_query_var('paged') ? get_query_var('paged') : 1,
             'post__in' => $results ? $results : array(0),
             'orderby' => wpbdp_get_option( 'listings-order-by', 'date' ),
